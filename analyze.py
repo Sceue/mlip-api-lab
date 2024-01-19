afbce73c8773729079d60247bbcbd8f051de804c
徐𤋮 <xu@MacBook-Pro-290.local>
2024-01-19 02:27:00 -0500

    load credentials

--- analyze.py
+++ analyze.py
@@ -2,9 +2,13 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import json
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+endpoint = config["AZURE_KEYVAULT_ENDPOINT"]
+key = config["AZURE_KEYVAULT_KEY"]
 
 credentials = CognitiveServicesCredentials(key)
 
